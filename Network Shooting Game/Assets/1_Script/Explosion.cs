d8d96a32ca6ce10bb1a5168f987d1234ef93ff3f
neolgu <32592676+neolgu@users.noreply.github.com>
2017-11-30 16:33:14 +0900

    shoot game
    add bomb!
    explosion!!!!!!!!

---
+++ Network Shooting Game/Assets/1_Script/Explosion.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+    public float explosionPower = 500f;
+    public float explosionArea = 100f;
+    public float explosionUplift = 200f;
+
+    private Rigidbody2D bombRigidbody;
+
+    void Awake()
+    {
+        bombRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        if (!bombRigidbody.isKinematic)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Baam();
+                // add explosion particle
+                Destroy(this.gameObject);
+            }
+        }
+	}
+
+    void Baam()
+    {
+        Collider2D[] aroundBombs = Physics2D.OverlapCircleAll(transform.position, explosionArea);
+
+        foreach(Collider2D col in aroundBombs)
+        {
+            if (col.GetComponent<Rigidbody2D>() != null)
+                Rigidbody2DExtension.AddExplosionForce(col.GetComponent<Rigidbody2D>(), explosionPower, transform.position, explosionArea, explosionUplift);
+        }
+    }
+
+
+}
