b22542eb8be62b89fb31fe8e53f176cb3d5af9d1
neolgu <32592676+neolgu@users.noreply.github.com>
2017-12-02 19:29:15 +0900

    NetShoot
    add UI & screen

---
+++ Network Shooting Game/Assets/1_Script/unstage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class unstage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MapMgr.self.stageNum = 2;
+	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            SceneManager.LoadScene("Map");
+    }
+}
