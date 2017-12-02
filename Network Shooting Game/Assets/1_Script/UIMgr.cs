26d6c339ab843fc8f4bc781bbfd766c6302bd794
neolgu <32592676+neolgu@users.noreply.github.com>
2017-12-02 16:13:48 +0900

    netshoot
    add UI

---
+++ Network Shooting Game/Assets/1_Script/UIMgr.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UIMgr : MonoBehaviour {
+    
+    public void OnClickMenuButton(GameObject menu)
+    {
+        menu.SetActive(!menu.active);
+
+        if (menu.active)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+
+    public void OnClickGoMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OnClickGoMap()
+    {
+        SceneManager.LoadScene("Map");
+    }
+}
