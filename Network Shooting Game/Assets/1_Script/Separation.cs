db9b420a2758b7fd0a38fae21e8fd25dcee7d451
neolgu <32592676+neolgu@users.noreply.github.com>
2017-12-11 00:32:59 +0900

    shoot game
    connect stage scene

--- Network Shooting Game/Assets/1_Script/Separation.cs
+++ Network Shooting Game/Assets/1_Script/Separation.cs
@@ -9,28 +9,30 @@ public class Separation : MonoBehaviour {
     private GameObject up;
     private GameObject mid;
     private GameObject down;
+    private Rigidbody2D my;
 
 	// Use this for initialization
 	void Start () {
-		
+        my = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(1))
-        {
-            up = Instantiate(mini, transform.position + new Vector3(1, 0, 0), Quaternion.identity);
-            mid = Instantiate(mini, transform.position, Quaternion.identity);
-            down = Instantiate(mini, transform.position + new Vector3(-1, 0, 0), Quaternion.identity);
+        if (!my.isKinematic)
+            if (Input.GetMouseButtonDown(1))
+            {
+                up = Instantiate(mini, transform.position + new Vector3(1, 0, 0), Quaternion.identity);
+                mid = Instantiate(mini, transform.position, Quaternion.identity);
+                down = Instantiate(mini, transform.position + new Vector3(-1, 0, 0), Quaternion.identity);
 
-            up.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity;
-            mid.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity;
-            down.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity;
+                up.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity;
+                mid.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity;
+                down.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity;
 
-            up.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, updownPower));
-            down.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -updownPower));
+                up.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, updownPower));
+                down.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -updownPower));
 
-            Destroy(this.gameObject);
-        }
+                Destroy(this.gameObject);
+            }
 	}
 }
