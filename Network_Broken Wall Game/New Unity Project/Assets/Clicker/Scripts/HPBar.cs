535ea8ed980c1847d80e5a64500852ace16025b2
neolgu <32592676+neolgu@users.noreply.github.com>
2017-12-16 19:31:41 +0900

    wall game
    first upload

---
+++ Network_Broken Wall Game/New Unity Project/Assets/Clicker/Scripts/HPBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Clicker {
+	public class HPBar : MonoBehaviour {
+		private SpriteRenderer spriteRenderer;
+		private float initialScale;
+
+		private void Awake() {
+			spriteRenderer = GetComponent<SpriteRenderer>();
+			initialScale = transform.localScale.x;
+		}
+
+		public void SetHPBar(float percent) {
+			if (percent < 0) percent = 0;
+			Vector3 newScale = transform.localScale;
+			newScale.x = percent * initialScale;
+			transform.localScale = newScale;
+		}
+	}
+}
